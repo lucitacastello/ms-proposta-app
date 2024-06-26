e2f928d6ce7081bf9dc33df57196105d8de0f206
cidarosa <lucita.rosa@gmail.com>
2024-06-26 13:53:42 -0300

    configurando Schedule

--- src/main/java/com/github/lucitacastello/proposta_app/agendador/PropostaSemIntegracao.java
+++ src/main/java/com/github/lucitacastello/proposta_app/agendador/PropostaSemIntegracao.java
@@ -1,12 +1,22 @@
 package com.github.lucitacastello.proposta_app.agendador;
 
+import com.github.lucitacastello.proposta_app.entities.Proposta;
 import com.github.lucitacastello.proposta_app.repository.PropostaRepository;
 import com.github.lucitacastello.proposta_app.service.NotificacaoRabbitService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class PropostaSemIntegracao {
+
+    //implementação que trabalha com Logger
+    private final Logger logger = LoggerFactory.getLogger(PropostaSemIntegracao.class);
 
     private PropostaRepository propostaRepository;
 
@@ -23,16 +33,24 @@ public class PropostaSemIntegracao {
         this.exchange = exchange;
     }
 
+    //agendador de tarefas
+    @Scheduled(fixedDelay = 10, timeUnit = TimeUnit.SECONDS)
     public void buscarPropstasSemIntegracao() {
 
         propostaRepository.findAllByIntegradaIsFalse().forEach(proposta -> {
             try {
                 notificacaoRabbitService.notificar(proposta, exchange);
-                proposta.setIntegrada(true);
-                propostaRepository.save(proposta);
+                atualizarProposta(proposta);
             } catch (RuntimeException ex) {
-                System.out.println(ex);
+                logger.error(ex.getMessage());
             }
         });
+    }
+
+    //refatorando
+    private void atualizarProposta(Proposta proposta) {
+
+        proposta.setIntegrada(true);
+        propostaRepository.save(proposta);
     }
 }
